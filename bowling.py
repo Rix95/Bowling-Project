bb7dd32f2107d8baa1231bf842800389985a6376
Rix95 <ricalrax@gmail.com>
2023-09-21 16:34:16 -0500

    first commit

---
+++ bowling.py
@@ -0,0 +1,13 @@
+class Bowling:
+    def __init__(self):
+        self.score = 0
+        self.round_throws = []
+        self.current_round = 0
+
+    def throw(self, pins):
+        # check if the game is over pending
+
+        return
+
+    def update_score(self):
+        return
